90612e6c361bd918999c4010b0f8ed3e3769f935
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-17 14:22:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,7 +25,7 @@ then
     #  sudo mkdir -p /etc/apt/keyrings
     #  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     
-    # # 设置 Docker 仓库
+    # # # 设置 Docker 仓库
     # echo \
     #   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     #   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
