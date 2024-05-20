44709f5e4961e657b26a58b8e1b97d716581d47f
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-21 01:09:35 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,3 @@
-echo $a
 screen -X -S pingpong quit
-read -p "请输入你的key device id: " your_device_id
-keyid="$your_device_id"
+keyid=$device_id
 screen -dmS pingpong bash -c "./PINGPONG --key \"$keyid\""
