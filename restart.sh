06afb5164aa8dcc25d8927fa148258494bc0dbc6
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-22 09:23:04 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,5 +2,6 @@ screen -X -S pingpong quit
 read -p "请输入你的key device id: " device_id
 keyid=$device_id
 screen -dmS pingpong bash -c "./PINGPONG --key \"$keyid\""
+echo '已重启，请返回我的服务，点击检查状态'
 cd ~
 rm -f restart.sh
