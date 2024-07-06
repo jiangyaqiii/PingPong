1e3343718271cbbf4fba833ecfecc108ba3abde2
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-06 11:06:42 +0800

    Update start_aioz.sh

--- start_aioz.sh
+++ start_aioz.sh
@@ -2,3 +2,5 @@ apt install curl
 curl -LO https://github.com/AIOZNetwork/aioz-dcdn-cli-node/files/13561211/aioznode-linux-amd64-1.1.0.tar.gz
 tar xzf aioznode-linux-amd64-1.1.0.tar.gz
 mv aioznode-linux-amd64-1.1.0 aioznode
+./aioznode version
+./aioznode keytool new --save-priv-key privkey.json
